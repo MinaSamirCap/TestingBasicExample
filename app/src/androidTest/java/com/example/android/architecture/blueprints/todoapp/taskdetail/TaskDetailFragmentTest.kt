bf4ab2331d6c0e955eea72cac086f7d4b0143878
MinaSamir <menasamercap@gmail.com>
2020-06-26 12:44:18 +0200

    finished Service locator and displyaed the task correctly in the task details fragment

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,23 +1,44 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
+import FakeAndroidTestRepository
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.*
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 @MediumTest
-class TaskDetailFragmentTest{
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class TaskDetailFragmentTest {
 
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun cleanUpDB() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
 
     @Test
-    fun activeTaskDetails_DisplayedInUi(){
+    fun activeTaskDetails_DisplayedInUi() = runBlockingTest {
         // GIVEN - add active (in complete task to the DB)
         val activeTask = Task("Active task", "AndroidX Rocks", false)
+
+        repository.saveTask(activeTask)
 
         // WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,20 +9,27 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
 object ServiceLocator {
+
+    private val lock = Any()
+
     private var database: ToDoDatabase? = null
+
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting set
 
-    fun provideTasksRepository(context: Context):TasksRepository{
-     synchronized(this){
-         return tasksRepository?: createTasksRepository(context)
-     }
+    fun provideTasksRepository(context: Context): TasksRepository {
+        synchronized(this) {
+            return tasksRepository ?: createTasksRepository(context)
+        }
     }
 
     private fun createTasksRepository(context: Context): TasksRepository {
-        val newRepo = DefaultTasksRepository(TasksRemoteDataSource, createTaskLocalDataSource(context))
+        val newRepo =
+            DefaultTasksRepository(TasksRemoteDataSource, createTaskLocalDataSource(context))
         tasksRepository = newRepo
         return newRepo
     }
@@ -38,5 +46,21 @@ object ServiceLocator {
         ).build()
         database = result
         return result
+    }
+
+    @VisibleForTesting
+    fun resetRepository(){
+        synchronized(lock){
+            runBlocking {
+                TasksRemoteDataSource.deleteAllTasks()
+            }
+            // clear all data to avoid test pollution
+            database?.apply {
+                clearAllTables()
+                close()
+            }
+            database = null
+            tasksRepository = null
+        }
     }
 }
